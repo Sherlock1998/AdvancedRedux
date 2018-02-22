afd5f6d37831df8653b26c98e3aa43748d8efe34
Yoong Jian <iuncleblade_yj@hotmail.com>
2018-02-22 22:08:46 +0800

    secured /feature with a HOC

--- client-auth/src/actions/index.js
+++ client-auth/src/actions/index.js
@@ -17,7 +17,7 @@ export function signinUser({email,password}) {
     //Save JWT token
     localStorage.setItem('token',response.data.token);
     //redirect to /feature
-    browserHistory.push('/features');
+    browserHistory.push('/feature');
   })
   .catch(()=> {
     //if request is bad, show err msg
@@ -38,7 +38,7 @@ export function signupUser({email,password}) {
       .then(response => {
         dispatch({type:AUTH_USER});
         localStorage.setItem('token', response.data.token);
-        browserHistory.push('/features');
+        browserHistory.push('/feature');
       })
       .catch(response => {
         dispatch(authError(response.data.error));
